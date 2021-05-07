7ef4523a3e65a092e90a88f13e7a3959304b508e
roger-mcz <roger.corpore@gmail.com>
2021-05-07 07:12:55 -0300

    find, delete, up ById

--- src/main/java/local/rogerdom/rest/AlunoController.java
+++ src/main/java/local/rogerdom/rest/AlunoController.java
@@ -5,6 +5,9 @@ import local.rogerdom.repository.AlunoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+import javax.persistence.criteria.CriteriaBuilder;
 
 @RestController
 @RequestMapping("/api/alunos")
@@ -21,7 +24,39 @@ public class AlunoController {
     @ResponseStatus(HttpStatus.CREATED)
     public Aluno save(@RequestBody Aluno aluno){
         return alunoRepository.save(aluno);
-
     }
+
+    @GetMapping("{id}")
+    public Aluno findById( @PathVariable Integer id){
+        return alunoRepository
+                .findById(id)
+                .orElseThrow( ()-> new ResponseStatusException(HttpStatus.NOT_FOUND));
+    }
+
+    @DeleteMapping("{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteById(@PathVariable Integer id){
+        alunoRepository
+                .findById(id)
+                .map( aluno -> {
+                    alunoRepository.delete(aluno);
+                    return Void.TYPE;
+                })
+                .orElseThrow( ()-> new ResponseStatusException(HttpStatus.NOT_FOUND));
+    }
+
+    @PutMapping("{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public void updateById(@PathVariable Integer id, @RequestBody Aluno alunoUpdate){
+        alunoRepository
+                .findById(id)
+                .map( aluno -> {
+                    alunoUpdate.setId(aluno.getId());
+                    return alunoRepository.save(alunoUpdate);
+                })
+                .orElseThrow( ()-> new ResponseStatusException(HttpStatus.NOT_FOUND));
+    }
+
+
 
 }
